0b926a3d52be958613fb8a36d10021a0cb6c2bbe
LambethWalk <lavi.eyal@gmail.com>
2016-06-19 09:54:50 +0000

    Copied from PC

--- geo.php
+++ geo.php
@@ -1,6 +1,7 @@
 <?php
 //latlang with preferred zoom
 $locations = [
+		"Select location" => "",
 		"London" => "51.51,-0.14/11", 
 		"Ostuni" => "40.94,17.41/9",
 		"Islamabad" => "34.08,73.32/8"
@@ -9,7 +10,7 @@ $locations = [
 asort($locations);
 reset($locations);
 
-$control = '<select name="loc">';
+$control = '<select name="loc" onchange="validate(this.value);">';
 
 foreach($locations as $p => $w){
 	$control .= '<option value="'. $w .'">' .$p .'</option>';
@@ -25,17 +26,27 @@ $control .= '</select>';
 <meta name="referrer" content="no-referrer" /> <!-- Stop 403 from website -->
 <meta charset="UTF-8">
 <title></title>
+<link rel="stylesheet" type="text/css" href="styles.css">
+<script type="text/javascript">
+function validate(text)
+  {
+    if (text.length > 1){
+      document.getElementById("button").disabled = false;
+    }else{
+      document.getElementById("button").disabled = true;
+    }
+  }
+</script>
 </head>
 
 <body>
 	<form action="flights.php" method="get">		
-		<div id="label">
-
-		</div>
-		<div id="control">
+		<div>
+			<p>Where are you?</p>
 			<?= $control?>
+			<button id="button" disabled>Select</button>
 		</div>
-		<button>Select</button>
+		
 	</form>
 </body>
 
