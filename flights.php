aae6cf91db3c1806d34325b56f8f539a1199bd26
LambethWalk <lavi.eyal@gmail.com>
2016-07-03 12:45:24 +0000

    various

--- flights.php
+++ flights.php
@@ -1,19 +1,27 @@
 <?php
 $querystring = $_GET['loc'];
-header('Location: https://www.flightradar24.com/' . $querystring);
-exit;
-//disbale touch screen so that user can't click links (no way to go back)
-//exec('DISPLAY=:0 xinput disable 6');
 
-//requires 'export DISPLAY=:0' in /etc/profile
-
-
-//exec('sleep 10');
-
-//using a FF extension instead
-//exec('xte "key F11"');
-
-// debugging: note 2>&1
-//exec('DISPLAY=:0 xinput disable 6 2>&1', $output, $return_var);
+//exec('export DISPLAY=:0', $output, $ret);
 //var_dump($output);
-//var_dump($return_var);
+//echo ('export: ' . $ret . '<br/>');
+
+
+// remove X11 access control
+exec('DISPLAY=:0 sudo /usr/bin/xhost + 2>&1', $output, $ret);
+var_dump($output);
+echo ('xhost: ' . $ret . '<br/>');
+
+//disbale touch screen so that user can't click links (no way to go back)
+exec('DISPLAY=:0 sudo /usr/bin/xinput disable 6 2>&1', $output, $ret);
+var_dump($output);
+echo ('xinput: ' . $ret . '<br/>');
+
+//remove cursor
+exec('DISPLAY=:0 unclutter -idle 1 & 2>&1', $output, $ret);
+var_dump($output);
+echo ('unclutter: ' . $ret);
+
+//exec('sleep 5');
+
+//go to site
+header('Location: https://www.flightradar24.com/' . $querystring);
